c5091e64efe6cd526ae9959960056f1106a49536
Émile Jetzer <emile.jetzer@polymtl.ca>
2022-06-13 18:41:16 -0400

    Fichiers de configuration, entre autre pour Read The Docs

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Émile Jetzer'
 author = 'Émile Jetzer'
 
 # The full version, including alpha/beta/rc tags
-release = '3.2'
+release = '4'
 
 
 # -- General configuration ---------------------------------------------------
